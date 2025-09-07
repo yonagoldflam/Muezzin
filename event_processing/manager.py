d4c69b2ae8687f4eb72e3fecc6d0c9f66ac4170d
tzirkus <zalmen5779@gmail.com>
2025-09-07 16:48:58 +0300

    hash for id

--- event_processing/manager.py
+++ event_processing/manager.py
@@ -1,5 +1,17 @@
 from utils.kafka_configuration import consume_message
+import hashlib
+import json
 
 class Manager:
     def __init__(self):
         pass
+
+
+
+    def create_hash(self, document):
+        sort_document = json.dumps(document, sort_keys=True).encode('utf-8')
+        hashed_document = hashlib.sha256(sort_document).hexdigest()
+        return hash
+
+
+
