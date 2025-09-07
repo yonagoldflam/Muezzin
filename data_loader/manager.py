f106c35bdcd520114c7c36037b91122d24e144a2
tzirkus <zalmen5779@gmail.com>
2025-09-07 15:50:04 +0300

    fix structure

--- data_loader/manager.py
+++ data_loader/manager.py
@@ -1,23 +1,38 @@
-from data_loader import DataLoader
+import time
+from pathlib import Path
 from utils.kafka_configuration import produce_message, send_event
 
 class Manager:
     def __init__(self):
         self.directory_files_path = 'C:/podcasts'
-        self.data_loader = DataLoader(self.directory_files_path)
 
-    def get_meta_data(self):
-        return self.data_loader.meta_data()
-
-    def publish_message(self):
-        list_files = self.get_meta_data()
+    def run_files_and_publish_the_path_and_meta_data_to_kafka(self):
         producer = produce_message()
         topic = 'path_meta-data'
-        for document in list_files:
+        path = Path(self.directory_files_path)
+        for file in path.iterdir():
+            document = self.create_json_file_with_path_and_meta_data(file)
             send_event(producer, topic, document)
+
+
+
+    def create_json_file_with_path_and_meta_data(self,file : Path):
+        meta_data = self.create_meta_data(file)
+        document = {'file_path': str(file),
+                    'meta_data': meta_data}
+        return document
+
+    def create_meta_data(self, file : Path):
+        meta_data = {'name' : file.name,
+                     'size' : file.stat().st_size,
+                     'data_time' : time.ctime(file.stat().st_mtime)}
+
+        return meta_data
+
+
 
 
 
 if __name__ == '__main__':
     m = Manager()
-    m.publish_message()
+    m.run_files_and_publish_the_path_and_meta_data_to_kafka()
