40bef45ae26f8d6c2e63875d5bc89a658ac01bde
tzirkus <zalmen5779@gmail.com>
2025-09-07 12:10:57 +0300

    convert audio file to text

---
+++ data_loader/manager.py
@@ -0,0 +1,19 @@
+from convert_audo_to_text import Converter
+
+
+class Manager:
+    def __init__(self):
+        self.audio_file_path = 'C:/podcasts/download (1).wav'
+        self.model_name = 'distil-large-v3'
+        self.device = 'cpu'
+        self.computer_type = 'int8'
+        self.language = 'en'
+
+    def convert(self):
+        converter = Converter(audio_file_path=self.audio_file_path, model_name=self.model_name, device=self.device, computer_type=self.computer_type, language=self.language)
+        text = converter.transcribe()
+        print(text)
+
+if __name__ == '__main__':
+    m = Manager()
+    m.convert()
