f1bc53568047b334961a171ff5681e87cfeb74b5
tzirkus <zalmen5779@gmail.com>
2025-09-10 15:08:44 +0300

    fix text_processing structure

--- text_processing/src/manager.py
+++ text_processing/src/manager.py
@@ -1,7 +1,8 @@
 from utils.elastic_search.elastic_dal import ElasticDal
-from text_decoding import TextDecoding
 from utils.kafka_configuration.kafka_configuration import consume_messages
 from utils.logging.logger import Logger
+from text_processing.src.text_decoding import TextDecoding
+from text_processing.src.BDS_text_classification import BDSTextClassification
 
 logger = Logger().get_logger()
 
@@ -28,52 +29,20 @@ class Manager:
             document = event.value
             doc_id = document['id']
             text = document['text'].lower()
-            score = self.calculate_hostile_score(text)
+            score = BDSTextClassification.calculate_hostile_score(text, self.hostile_decoding_list, self.not_hostile_decoding_list)
             self.add_bds_field_to_document(doc_id, score)
             self.add_threat_level_field(doc_id, score)
 
 
     def add_bds_field_to_document(self, doc_id,score):
-        bds_field = self.is_bds(score)
+        bds_field = BDSTextClassification.is_bds(score)
         self.elastic_client.add_field_to_document(self.index, doc_id, bds_field)
         logger.info(f'field {bds_field} added to doc id:{doc_id} in elastic')
 
     def add_threat_level_field(self, doc_id, score):
-        threat_level_field = self.threat_level_field(score)
+        threat_level_field = BDSTextClassification.threat_level_field(score)
         self.elastic_client.add_field_to_document(self.index, doc_id, threat_level_field)
         logger.info(f'field {threat_level_field} added to doc id:{doc_id} in elastic')
-
-
-    def calculate_hostile_score(self, text):
-        score = 0
-        for hostile in self.hostile_decoding_list:
-            score += text.count(hostile) * 2
-        for not_hostile in self.not_hostile_decoding_list:
-            score += text.count(not_hostile)
-        if score:
-            return len(text) / score
-        return 0
-
-    def is_bds(self, score):
-        is_bds_field = {'is_bds': 'True'}
-        not_bds_field = {'is_bds': 'False'}
-        if score:
-            if score < 60:
-                return is_bds_field
-            else:
-                return not_bds_field
-        return not_bds_field
-
-    def threat_level_field(self, score):
-        high_threat_field = {'bds_threat_level': 'high'}
-        medium_threat_field = {'bds_threat_level': 'medium'}
-        none_threat_field = {'bds_threat_level': 'none'}
-        if score < 40:
-            return high_threat_field
-        elif score < 100:
-            return medium_threat_field
-        else:
-            return none_threat_field
 
 if __name__ == '__main__':
     manager = Manager()
